c2f60020aecbc1c59303ae0075a1ffb09153eea5
meriadec <meriadec.pillet@gmail.com>
2015-11-07 19:56:59 +0100

    refactor(build): use es6 syntax for build

---
+++ bin/build.js
@@ -0,0 +1,19 @@
+import webpack from 'webpack'
+import ProgressPlugin from 'webpack/lib/ProgressPlugin'
+import webpackConfig from '../webpack/production'
+
+const bundler = webpack(webpackConfig)
+
+const progressPlugin = new ProgressPlugin((percentage, info) => {
+  const msg = `${Math.round(percentage * 100)}% ${info}`
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  process.stdout.write(msg)
+})
+
+bundler.apply(progressPlugin)
+
+bundler.run((err, stats) => {
+  if (err) { return console.log(err) }
+  console.log(stats.toString(webpackConfig.stats))
+})
