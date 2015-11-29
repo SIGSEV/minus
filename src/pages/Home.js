3cd168f3ed6b3e5eb350fcac5ebdda357badb95b
Balthazar Gronon <bgronon@gmail.com>
2015-11-29 15:28:59 +0100

    style(demo): improve demo home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,21 +26,21 @@ class HomePage extends Component {
   }
 
   render () {
-    const { counter } = this.props
+    const { value, fetching } = this.props.counter
 
     return (
-      <div>
+      <div className='Home'>
 
-        <h1 className='Counter'>{counter.value}</h1>
+        <h1 className='Counter'>{fetching ? 'Loading...' : value}</h1>
 
-        <div>{'Actions can be quite simple'}</div>
-        <div>
-          <button onClick={::this.increment}>{'Increment'}</button>
-          <button onClick={::this.decrement}>{'Decrement'}</button>
+        <h3>{'Actions can be quite simple'}</h3>
+        <div className='zsa'>
+          <button onClick={::this.increment} disabled={fetching} className='DemoBtn'>{'Increment'}</button>
+          <button onClick={::this.decrement} disabled={fetching} className='DemoBtn'>{'Decrement'}</button>
         </div>
         <h3>{'But also asynchronous, like an API call.'}</h3>
-        <div>
-          <button onClick={::this.fetch}>{'Fetch'}</button>
+        <div className='z'>
+          <button onClick={::this.fetch} disabled={fetching} className='DemoBtn'>{'Fake Fetch'}</button>
         </div>
 
       </div>
