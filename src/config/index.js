6d5cb50673802fd19cbafbd7272be8f0d1bdd851
meriadec <meriadec.pillet@gmail.com>
2015-12-05 12:56:54 +0100

    refactor(core): move some files, write things into some others

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,16 @@
+import path from 'path'
+
 const env = process.env.NODE_ENV || 'development'
 const config = require(`./${env}`)
 
 export default {
+
   env,
   port: 3000,
+
+  assetsFolder: path.join(__dirname, '../assets'),
+  distFolder: path.join(__dirname, '../../dist'),
+
   ...config
+
 }
--- src/server.js
+++ src/server.js
@@ -1,28 +1,27 @@
-import path from 'path'
 import express from 'express'
 import compression from 'compression'
 
 import config from 'config'
+import render from 'middlewares/render'
+import devServer from 'middlewares/dev-server'
 
 delete process.env.BROWSER
 
 const server = express()
 
-server.use('/assets', express.static(path.join(__dirname, './assets')))
+server.use('/assets', config.assetsFolder)
 
 if (config.env === 'development') {
-  require('dev/server')(server)
+  devServer(server)
 }
 
 if (config.env === 'production') {
-  const render = require('middlewares/render')
-
   server.use(compression())
-  server.use('/dist', express.static(path.join(__dirname, '../dist')))
+  server.use('/dist', config.distFolder)
   server.use(render)
 }
 
 server.listen(config.port, 'localhost', err => {
   if (err) { return console.log(err) }
-  console.log(`listening at http://localhost:${config.port} in ${config.env} mode`)
+  console.log(`listening at localhost:${config.port} in ${config.env} mode`)
 })
