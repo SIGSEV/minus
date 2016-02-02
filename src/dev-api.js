1031152c8f7b8af412f2f2314266e6769978d662
meriadec <meriadec.pillet@gmail.com>
2016-02-02 20:26:53 +0100

    fix(dev-api): add missing header

--- src/dev-api.js
+++ src/dev-api.js
@@ -9,6 +9,7 @@ const server = express()
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', `http://localhost:${config.port}`)
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   next()
 })
 
