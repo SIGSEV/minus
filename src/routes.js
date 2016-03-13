af2c5d7d3dc48fc7b5a5e2ef4cd396c9e0b449cf
meriadec <meriadec.pillet@gmail.com>
2016-03-13 18:39:35 +0100

    refactor(core): remove opiniated choice from src folder

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from 'components/App'
-import Home from 'pages/Home'
+import Home from 'components/Home'
 
 export default (
   <Route path='/' component={App}>
