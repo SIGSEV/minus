4edb99fc8de127116bbae6a7301e2579155462d5
Balthazar Gronon <bgronon@gmail.com>
2016-03-14 21:19:37 +0100

    feat(client): fetch redial hooks on route update

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { syncHistoryWithStore } from 'react-router-redux'
+import { trigger } from 'redial'
 import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router'
 
@@ -10,9 +11,21 @@ import routes from 'routes'
 const store = createStore(browserHistory)
 const history = syncHistoryWithStore(browserHistory, store)
 
+function routerUpdate () {
+  const { components, location, params } = this.state // eslint-disable-line
+  const { dispatch } = store
+
+  trigger('fetch', components, {
+    path: location.pathname,
+    query: location.query,
+    params,
+    dispatch
+  })
+}
+
 const root = (
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <Router history={history} routes={routes} onUpdate={routerUpdate} />
   </Provider>
 )
 
