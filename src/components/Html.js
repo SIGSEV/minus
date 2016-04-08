802beba2a87dc692f0f58daa38acd3e7494d5bd0
meriadec <meriadec.pillet@gmail.com>
2016-04-08 23:14:50 +0200

    refactor(html): reformatting a bit

--- src/components/Html.js
+++ src/components/Html.js
@@ -9,13 +9,9 @@ const Html = ({ content, state, stats: { style, main = 'bundle.js' } }) => (
       <meta charSet='utf-8' />
       <link rel='icon' href='/assets/favicon.ico' type='image/x-icon' />
 
-      {style && (
-        <link href={`/dist/${style}`} rel='stylesheet' />
-      )}
+      {style && <link href={`/dist/${style}`} rel='stylesheet' />}
 
-      {state && (
-        <script dangerouslySetInnerHTML={{ __html: `window.__INITIAL_STATE__ = ${JSON.stringify(state)}` }} />
-      )}
+      <script dangerouslySetInnerHTML={{ __html: `window.__INITIAL_STATE__ = ${JSON.stringify(state)}` }} />
 
     </head>
     <body>
