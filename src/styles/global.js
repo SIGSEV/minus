37eb6e9f86825f9d10218fbf8dad37e95388acbb
meriadec <meriadec.pillet@gmail.com>
2018-03-17 15:03:00 +0100

    Style reset of padding/margin

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,6 +3,8 @@ import { injectGlobal } from 'styled-components'
 injectGlobal`
   *, *:after, *:before {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
     font: inherit;
     color: inherit;
   }
